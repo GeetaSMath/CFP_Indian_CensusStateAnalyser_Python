f06b19ebf7a83c911580e33a60c83b31f274c470
GeetaSMath <geetasmath@gmail.com>
2022-01-03 15:14:11 +0530

    Added:load csv file

--- indian_census_analyser.py
+++ indian_census_analyser.py
@@ -1,1 +1,11 @@
-"welcome to solve indian census analyser problem soluation"
+import pandas as pd
+
+def state_census_problem():
+    csv_data = pd.read_csv('C:/Users/Geetha S Matha/Desktop/IndianCensus - Sheet1.csv')
+    print(csv_data.shape)
+    print(csv_data)
+
+
+list = state_census_problem()
+
+print(list)
